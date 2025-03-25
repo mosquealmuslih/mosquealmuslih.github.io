5cfac0378f6350a76265abb21c0035d6240fa34c
mosquealmuslih <mosquealmuslih@gmail.com>
2025-03-25 15:02:22 +0700

    Update1

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6iNmZMtAp5UK4AAEVyq8BCfWkG7ZP88",
+  authDomain: "dbalmuslih.firebaseapp.com",
+  projectId: "dbalmuslih",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const dataContainer = document.getElementById("data-container");
 
-db.collection("datamasuk")
-  .get()
-  .then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      const data = doc.data();
-      const div = document.createElement("div");
-      div.innerHTML = `
+async function getData() {
+  const querySnapshot = await getDocs(collection(db, "datamasuk"));
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    const div = document.createElement("div");
+    div.innerHTML = `
       <hr>
       <p><strong>Nomor Rumah:</strong> ${data.nomor_rmh}</p>
       <p><strong>Nominal:</strong> Rp${data.nominal.toLocaleString()}</p>
       <p><strong>Tanggal Masuk:</strong> ${data.tanggal_masuk}</p>
     `;
-      dataContainer.appendChild(div);
-    });
+    dataContainer.appendChild(div);
   });
+}
+
+getData();
